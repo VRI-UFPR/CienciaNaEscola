bde86d13ddc5fe73a46d49961285898888b0995c
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-03-07 02:34:30 -0300

    [ADD] Componente RadioButtonInput

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TermsPage from "./pages/TermsPage";
 import EndProtocolPage from "./pages/EndProtocolPage";
 import ProfilePage from "./pages/ProfilePage";
+import RadioButtonInput from "./components/RadioButtonInput";
 
 const styles = `
 .App {
@@ -48,34 +49,35 @@ const styles = `
 `;
 
 function App(props) {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <div className="App">
-                            <header className="App-header">
-                                <img src={logo} className="App-logo" alt="logo" />
-                                <p>
-                                    Edit <code>src/App.js</code> and save to reload.
-                                </p>
-                                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                                    Learn React
-                                </a>
-                            </header>
-                            <style dangerouslySetInnerHTML={{ __html: styles }} />
-                        </div>
-                    }
-                />
-                <Route path="/splash" element={<SplashPage />} />
-                <Route path="/terms" element={<TermsPage />} />
-                <Route path="/endprotocol" element={<EndProtocolPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-            </Routes>
-            <style dangerouslySetInnerHTML={{ __html: styles }} />
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<div className="App">
+							<header className="App-header">
+								<img src={logo} className="App-logo" alt="logo" />
+								<p>
+									Edit <code>src/App.js</code> and save to reload.
+								</p>
+								<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+									Learn React
+								</a>
+							</header>
+							<style dangerouslySetInnerHTML={{ __html: styles }} />
+						</div>
+					}
+				/>
+				<Route path="/splash" element={<SplashPage />} />
+				<Route path="/terms" element={<TermsPage />} />
+				<Route path="/endprotocol" element={<EndProtocolPage />} />
+				<Route path="/profile" element={<ProfilePage />} />
+				<Route path="/radiooptions" element={<RadioButtonInput options={["Área de plantação", "Jardim", "Praça", "Escola"]} />} />
+			</Routes>
+			<style dangerouslySetInnerHTML={{ __html: styles }} />
+		</BrowserRouter>
+	);
 }
 
 export default App;
