e7386da116b69bc8aa34afd44202dd05b2c9805e
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-03-17 10:46:28 -0300

    [FIX] Indentação padronizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import logo from "./assets/images/logo.svg";
-import React from "react";
-import SplashPage from "./pages/SplashPage";
-import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TermsPage from "./pages/TermsPage";
-import EndProtocolPage from "./pages/EndProtocolPage";
-import ProfilePage from "./pages/ProfilePage";
-import RadioButtonInput from "./components/RadioButtonInput";
-import SimpleTextInput from "./components/SimpleTextInput";
-import InfoGerais from "./components/InfoGerais";
-import DateInput from "./components/DateInput";
-import TimeInput from "./components/TimeInput";
+import logo from './assets/images/logo.svg';
+import React from 'react';
+import SplashPage from './pages/SplashPage';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TermsPage from './pages/TermsPage';
+import EndProtocolPage from './pages/EndProtocolPage';
+import ProfilePage from './pages/ProfilePage';
+import RadioButtonInput from './components/RadioButtonInput';
+import SimpleTextInput from './components/SimpleTextInput';
+import InfoGerais from './components/InfoGerais';
+import DateInput from './components/DateInput';
+import TimeInput from './components/TimeInput';
 
 const styles = `
 .App {
@@ -54,40 +54,48 @@ const styles = `
 `;
 
 function App(props) {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<div className="App">
-							<header className="App-header">
-								<img src={logo} className="App-logo" alt="logo" />
-								<p>
-									Edit <code>src/App.js</code> and save to reload.
-								</p>
-								<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-									Learn React
-								</a>
-							</header>
-							<style dangerouslySetInnerHTML={{ __html: styles }} />
-						</div>
-					}
-				/>
-        <Route path="/navbar" element={<Navbar />} />
-				<Route path="/splash" element={<SplashPage />} />
-				<Route path="/terms" element={<TermsPage />} />
-				<Route path="/endprotocol" element={<EndProtocolPage />} />
-				<Route path="/profile" element={<ProfilePage />} />
-				<Route path="/simpletext" element={<SimpleTextInput />} />
-				<Route path="/info" element={<InfoGerais />} />
-				<Route path="/date" element={<DateInput />} />
-				<Route path="/time" element={<TimeInput />} />
-				<Route path="/radiooptions" element={<RadioButtonInput options={["Área de plantação", "Jardim", "Praça", "Escola"]} />} />
-			</Routes>
-			<style dangerouslySetInnerHTML={{ __html: styles }} />
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div className="App">
+                            <header className="App-header">
+                                <img src={logo} className="App-logo" alt="logo" />
+                                <p>
+                                    Edit <code>src/App.js</code> and save to reload.
+                                </p>
+                                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                                    Learn React
+                                </a>
+                            </header>
+                            <style
+                                dangerouslySetInnerHTML={{
+                                    __html: styles,
+                                }}
+                            />
+                        </div>
+                    }
+                />
+                <Route path="/navbar" element={<Navbar />} />
+                <Route path="/splash" element={<SplashPage />} />
+                <Route path="/terms" element={<TermsPage />} />
+                <Route path="/endprotocol" element={<EndProtocolPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/simpletext" element={<SimpleTextInput />} />
+                <Route path="/info" element={<InfoGerais />} />
+                <Route path="/date" element={<DateInput />} />
+                <Route path="/time" element={<TimeInput />} />
+                <Route path="/radiooptions" element={<RadioButtonInput options={['Área de plantação', 'Jardim', 'Praça', 'Escola']} />} />
+            </Routes>
+            <style
+                dangerouslySetInnerHTML={{
+                    __html: styles,
+                }}
+            />
+        </BrowserRouter>
+    );
 }
 
 export default App;
