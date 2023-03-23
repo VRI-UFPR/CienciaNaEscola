f3488228e52d445abf3bfe66ffc4bc70057c02cf
YuriJT <Yuri.r.tobias@hotmail.com.br>
2023-03-23 07:57:54 -0300

    [ADD] arquivo Location.jsx

---
+++ src/components/Location.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const styles = ``;
+
+function Location(props) {
+    return (
+        <div>
+            <div></div>
+            <style dangerouslySetInnerHTML={{ __html: styles }} />
+        </div>
+    );
+}
+
+export default Location;
