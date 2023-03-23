30089a5d908adc9d6dacfa091509393b8e1fe979
Juliana Zambon <zambon.juju@gmail.com>
2023-03-23 10:19:27 -0300

    [ADD] estilização de alguns items

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,22 +2,62 @@ import React from "react";
 import LoginTitle from "../assets/images/loginTitle.svg";
 import Background from "../assets/images/paginaInicialLogin.svg";
 
-const styles = `.login-wrapper {
+const styles = `
+    .login-wrapper {
     background-size: 100%;
     background-repeat: repeat-y;
-}`;
+    }
+
+    .login-title{
+        text-align: center;
+    }
+
+    .login-input{
+        width: 70%;
+        height: 30px;
+        background-color: #4E9BB9;
+        color: #FFFFFF;
+        font-size: 90%;
+        border-radius: 50px;
+        border: 0px;
+    }
+
+    ::placeholder {
+        color: #FFFFFF;
+    }
+
+    .login-button {
+        width: 70%;
+        height: 30px;
+        background-color: #AAD390;
+        color: #FFFFFF;
+        border-radius: 50px;
+        border: 0px;
+
+    }
+`;
 
 function LoginPage(props) {
     return (
-        <div style={{ backgroundImage: `url(${Background})`, width: "718px", height: "1349px" }}>
+        <div>
             <div className="login-wrapper d-flex flex-column">
-                <div className="d-flex align-items-center">
-                    <img src={LoginTitle} alt="" style={{ width: "100px", height: "100px" }} />
+                <div className="d-flex justify-content-center">
+                    <img src={LoginTitle} alt="" style={{ width: "250px", height: "250px" }} />
                 </div>
-                <span>Bem-Vindo ao Ciência Cidadã na Escola!</span>
-                <input type="text" placeholder="Login" />
-                <input type="text" placeholder="Senha" />
-                <button> Entrar </button>
+                <div className="d-flex justify-content-center">
+                    <span className="login-title">
+                        Bem-vindo(a) ao <br /> Ciência Cidadã na <br />
+                        Escola!
+                    </span>
+                </div>
+                <div className=" d-flex flex-column align-items-center">
+                    <input className="login-input px-3" type="text" placeholder="Login" />
+                    <input className="login-input px-3" type="text" placeholder="Senha" />
+                </div>
+
+                <div className="d-flex justify-content-center">
+                    <button className="login-button "> Entrar </button>
+                </div>
             </div>
             <style dangerouslySetInnerHTML={{ __html: styles }} />
         </div>
