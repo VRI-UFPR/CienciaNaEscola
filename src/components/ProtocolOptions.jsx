3078681fe7ad182fe871b8e4c21b88b4c49dca66
Yuri Junqueira Tobias <Yuri.r.tobias@hotmail.com.br>
2023-03-27 14:15:46 -0300

    [FIX] correção na estilização de ProtocolOptions

--- src/components/ProtocolOptions.jsx
+++ src/components/ProtocolOptions.jsx
@@ -4,13 +4,13 @@ import SettingsIcon from "../assets/images/settingsIcon.svg";
 import CollaboratorsIcon from "../assets/images/collaboratorsIcon.svg";
 
 const styles = `
-    button {
+    .options-button {
         border: 0px;
         background-color: transparent;
         padding: 4px 4px;
     }
 
-    img {
+    .options-img {
         height: 20px;
     }
 `;
@@ -19,14 +19,14 @@ function ProtocolOptions(props) {
     return ( 
         <div>
             <div className="wrapper d-flex justify-content-center py-1">
-                <button>
-                    <img src={EyeIcon} alt="eye icon" />
+                <button className="options-button">
+                    <img className="options-img" src={EyeIcon} alt="eye icon" />
                 </button>
-                <button>
-                    <img src={SettingsIcon} alt="settings icon" />
+                <button className="options-button">
+                    <img className="options-img" src={SettingsIcon} alt="settings icon" />
                 </button>
-                <button>
-                    <img src={CollaboratorsIcon} alt="collaborators icon" />
+                <button className="options-button">
+                    <img className="options-img" src={CollaboratorsIcon} alt="collaborators icon" />
                 </button>
             </div>
             <style dangerouslySetInnerHTML={{ __html: styles }} />
