d900248cf41e95ab7e36a99d6bf3a11d9a0398ab
eduardaafreitas <eduardaaguiar1012@hotmail.com>
2023-03-30 11:25:01 -0300

    [ADD] Pagina LogoutPage

---
+++ src/pages/LogoutPage.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import logoutTitle from '../assets/images/helpIcon.svg';
+import NavBar from '../components/Navbar';
+
+const styles = `
+
+    .bg-crimson {
+        background-color: #EC6571;
+    }
+
+    .font-barlow {
+        font-family: 'Barlow', sans-serif;
+    }
+
+    .font-century-gothic {
+        font-family: 'Century Gothic', sans-serif;
+    }
+
+    h1 {
+        color: #535353;
+        font-weight: bold;
+        font-size: x-large;
+    }
+
+    h2 {
+        color: #535353;
+        font-weight: 600;
+        font-size: medium;
+        text-align: justify;
+    }
+
+    .green-button {
+        border-radius: 10px;
+        background-color: #AAD390;
+        color: #FFF;
+        font-weight: 700;
+        font-size: 130%;
+    }
+`;
+
+
+function logoutpage(props){
+    return (
+        <div>
+            <div className="d-flex flex-column vh-100 w-100 bg-crimson font-barlow">
+            <NavBar />
+            
+    )
+}
