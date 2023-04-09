0334f073f7148ec84e89759488c0b78d43ceb200
YuriJT <Yuri.r.tobias@hotmail.com.br>
2023-04-09 11:53:43 -0300

    [FIX] compatibilidade de App.jsx e index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const styles = `
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
-  }
-`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
-        <style> {styles} </style>
+        <HashRouter>
+            <App />
+        </HashRouter>
     </React.StrictMode>
 );
 
