0ab23cf94f95161af3bb52cf87ba278afbd8eef7
YuriJT <Yuri.r.tobias@hotmail.com.br>
2023-04-09 11:53:43 -0300

    [FIX] compatibilidade de App.jsx e index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
@@ -11,64 +11,20 @@ import EndProtocolPage from './pages/EndProtocolPage';
 import HelpPage from './pages/HelpPage';
 import AboutPage from './pages/AboutPage';
 
-const styles = `
-.App {
-    text-align: center;
-  }
-  
-  .App-logo {
-    height: 40vmin;
-    pointer-events: none;
-  }
-  
-  @media (prefers-reduced-motion: no-preference) {
-    .App-logo {
-      animation: App-logo-spin infinite 20s linear;
-    }
-  }
-  
-  .App-header {
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-  }
-  
-  .App-link {
-    color: #61dafb;
-  }
-  
-  @keyframes App-logo-spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }  
-`;
-
 function App(props) {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<SplashPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/navbar" element={<Navbar />} />
-                <Route path="/terms" element={<TermsPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/protocol" element={<ProtocolPage />} />
-                <Route path="/endprotocol" element={<EndProtocolPage />} />
-                <Route path="/help" element={<HelpPage />} />
-                <Route path="/about" element={<AboutPage />} />
-            </Routes>
-            <style> {styles} </style>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<SplashPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/navbar" element={<Navbar />} />
+            <Route path="/terms" element={<TermsPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/protocol" element={<ProtocolPage />} />
+            <Route path="/endprotocol" element={<EndProtocolPage />} />
+            <Route path="/help" element={<HelpPage />} />
+            <Route path="/about" element={<AboutPage />} />
+        </Routes>
     );
 }
 
