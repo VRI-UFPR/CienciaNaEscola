0a9a2147e1c23a83248c8bed843c6f8c5ce0110f
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-13 18:39:22 -0300

    [FIX] Revisões dos componentes de input RadioButton e SimpleText

--- src/components/RadioButtonInput.jsx
+++ src/components/RadioButtonInput.jsx
@@ -5,36 +5,42 @@ const styles = `
         font-family: 'Barlow', sans-serif;
     }
 
-    .form-label, .form-check-label{
-        font-weight: 500;
+    .radio-button-label{
+        font-weight: 600;
         font-size: 90%;
         color: #535353;
     }
 
-    .form-check-input{
+    .radio-button-input{
         background-color: #D9D9D9
     }
 `;
 
 function RadioButtonInput(props) {
     return (
-        <div className="p-3 shadow rounded bg-white pb-4">
-            <div className="row m-0 justify-content-between mb-2">
-                <p className="form-label font-barlow lh-sm px-0">{props.input.question}</p>
+        <div className="shadow rounded bg-white p-3">
+            <div className="row justify-content-between m-0">
+                <p className="form-label radio-button-label font-barlow lh-sm px-0">{props.input.question}</p>
             </div>
+            <div className="pt-2">
+                {props.input.sugestions.map((option) => {
+                    const optname = option.value.toLowerCase().replace(/\s/g, '');
 
-            {props.input.sugestions.map((option) => {
-                const optname = option.value.toLowerCase().replace(/\s/g, '');
-
-                return (
-                    <div key={optname + 'input'} className="form-check ms-2 mb-2">
-                        <input className="form-check-input" type="radio" name="radiooptions" id={optname + 'input'}></input>
-                        <label className="form-check-label font-barlow" htmlFor={optname + 'input'}>
-                            {option.value}
-                        </label>
-                    </div>
-                );
-            })}
+                    return (
+                        <div key={optname + 'input'} className="form-check ms-2 mb-2">
+                            <input
+                                className="form-check-input radio-button-input"
+                                type="radio"
+                                name="radiooptions"
+                                id={optname + 'input'}
+                            ></input>
+                            <label className="form-check-label radio-button-label font-barlow" htmlFor={optname + 'input'}>
+                                {option.value}
+                            </label>
+                        </div>
+                    );
+                })}
+            </div>
             <style>{styles}</style>
         </div>
     );
--- src/components/SimpleTextInput.jsx
+++ src/components/SimpleTextInput.jsx
@@ -5,15 +5,19 @@ const styles = `
         font-family: 'Barlow', sans-serif;
     }
 
-    label{
-        font-weight: 500;
-        font-size: 90%;
+    .simple-text-label{
+        font-weight: 600;
+        font-size: 1rem;
         color: #535353;
     }
 
-    .form-control{
+    .simple-text-input, .simple-text-input::placeholder{
+        font-weight: 600;
         color: #787878;
-        font-size: 85%;
+        font-size: 1rem;
+    }
+
+    .simple-text-input{
         border-radius: 0;
         border: 0px;
         border-bottom: 1px solid #C1C1C1;
@@ -22,14 +26,19 @@ const styles = `
 
 function SimpleTextInput(props) {
     return (
-        <div className="shadow rounded bg-white pb-4 p-3">
+        <div className="shadow rounded bg-white p-3">
             <div className="row justify-content-between mb-1 m-0">
-                <label labelfor="simpletextinput" className="form-label font-barlow px-0">
+                <label labelfor="simpletextinput" className="form-label simple-text-label font-barlow px-0">
                     {props.input.question}
                 </label>
             </div>
 
-            <input type="text" className="form-control p-0 mb-4" id="simpletextinput" placeholder="Digite sua resposta aqui"></input>
+            <input
+                type="text"
+                className="form-control font-barlow simple-text-input p-0 mb-4"
+                id="simpletextinput"
+                placeholder="Digite sua resposta aqui"
+            ></input>
             <style>{styles}</style>
         </div>
     );
