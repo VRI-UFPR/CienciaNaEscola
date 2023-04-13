a1ffb2e8b327523e3fb03dd647c810ee0689d300
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-13 17:19:51 -0300

    [FIX] Componente timeInput com novo leiaute

--- src/components/TimeInput.jsx
+++ src/components/TimeInput.jsx
@@ -1,82 +1,59 @@
 import React from "react";
-import iconTime from "../assets/images/iconTime.svg";
+import iconTime from "../assets/images/iconTime.png";
 
 const styles = `
     .font-barlow {
         font-family: 'Barlow', sans-serif;
     }
+    .font-century-gothic {
+        font-family: 'Century Gothic', sans-serif;
+    }
 
-    label{
-        font-weight: 500;
-        font-size: 90%;
+    .bg-pastel-blue {
+        background-color: #91CAD6;
+    }
+
+    .time-label{
+        font-size: 1.1rem;
+        font-weight: 700;
         color: #535353;
     }
 
-	.bg-coral{
-		background-color: #FF9551;
-	}
-
-    .form-control{
+    .time-input{
+        font-size: 1.1rem;
         font-weight: 500;
         color: #787878;
-        font-size: 90%;
-        border-radius: 0;
-        border: 0px;
-        border-bottom: 1px solid #C1C1C1;
+        border: 0;
     }
 
-	.btn{
-        padding: 12%;
-        min-height: 0px;
-        line-height: 0px;
+    .time-icon{
+        max-width: 50px;
     }
-
-	.date-control{
-		border-bottom: 0;
-	}
-
-	.icon-col{
-		max-width: 40px;
-	}
 `;
 
 function TimeInput(props) {
-	return (
-		<div className="p-3 shadow rounded">
-			<div className="row m-0 justify-content-between mb-1">
-				<div className="col-1 d-flex justify-content-end p-0 pt-1 icon-col">
-					<div className="col d-flex p-0 align-items-start">
-						<button type="button" class="btn rounded-circle bg-coral w-100">
-							<img src={iconTime} alt="Ícone" className="w-100"></img>
-						</button>
-					</div>
-				</div>
-				<div className="col p-0 ps-2 lh-sm">
-					<label labelfor="timeinput" className="form-label font-barlow lh-sm m-0">
-						Horário da coleta
-					</label>
-					<input
-						type="text"
-						className="form-control p-0 lh-sm font-barlow"
-						id="descriptioninput"
-						placeholder="Adicionar descrição"
-					></input>
-				</div>
-			</div>
-			<div className="row m-0 d-flex justify-content-end pt-2">
-				<div className="col-1 icon-col"></div>
-				<div className="col ps-2">
-					<input
-						type="time"
-						className="form-control date-control font-barlow p-0 lh-sm d-inline-block w-auto"
-						id="timeinput"
-					></input>
-				</div>
-			</div>
-
-			<style>{styles}</style>
-		</div>
-	);
+    return (
+        <div className="rounded shadow bg-white overflow-hidden font-barlow p-0">
+            <div className="row overflow-hidden m-0">
+                <div className="col-2 d-flex bg-pastel-blue p-0">
+                    <div class="time-icon ratio ratio-1x1 w-50 align-self-center mx-auto">
+                        <img src={iconTime} alt="" />
+                    </div>
+                </div>
+                <div className="col py-3">
+                    <div className="row m-0">
+                        <label labelfor="timeinput" className="form-label time-label font-century-gothic m-0 ps-2 p-0">
+                            Data da coleta
+                        </label>
+                    </div>
+                    <div className="row m-0">
+                        <input type="time" className="form-control time-input w-auto ps-2 pt-1 p-0" id="timeinput"></input>
+                    </div>
+                </div>
+            </div>
+            <style>{styles}</style>
+        </div>
+    );
 }
 
 export default TimeInput;
