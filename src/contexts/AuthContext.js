7d98fdc3006f0e14e51e945f52ad8d444e5acc80
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-13 08:34:50 -0300

    [ADD] AuthProvider usando LocalStorage do navegador

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
@@ -8,8 +8,16 @@ export const AuthProvider = ({ children }) => {
         token: null,
     });
 
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem('user'));
+        if (storedUser) {
+            setUser(storedUser);
+        }
+    }, []);
+
     const login = (id, token) => {
         setUser({ id, token });
+        localStorage.setItem('user', JSON.stringify({ id, token }));
     };
 
     const logout = () => {
@@ -17,6 +25,7 @@ export const AuthProvider = ({ children }) => {
             id: null,
             token: null,
         });
+        localStorage.removeItem('user');
     };
 
     return <AuthContext.Provider value={{ user, login, logout }}>{children}</AuthContext.Provider>;
