cbeb50c26a74e6b46b1085873bf331658c418be6
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-14 19:03:07 -0300

    [FIX] Refatorações e eliminação de estilização não usada

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -4,10 +4,6 @@ import logoPicceTextual from '../assets/images/logoPicceTextual.svg';
 import ColoredBorder from '../components/ColoredBorder';
 
 const styles = `
-    .font-barlow {
-        font-family: 'Barlow', sans-serif;
-    }
-
     .logo-picce-circular{
         max-width: 350px;
     }
@@ -24,18 +20,18 @@ const styles = `
 
 function SplashPage(props) {
     return (
-        <div className="container-fluid d-flex flex-column font-barlow vh-100 px-0">
+        <div className="d-flex flex-column align-items-center vh-100">
             <ColoredBorder />
-            <div className="row flex-grow-1 align-items-end w-75 mx-auto ">
-                <img src={logoPicceCircular} className="mx-auto logo-picce-circular" alt="Logo gráfico Picce"></img>
+            <div className="row flex-grow-1 align-items-end justify-content-center w-75 m-0">
+                <img src={logoPicceCircular} className="logo-picce-circular" alt="Logo gráfico Picce"></img>
             </div>
-            <div className="row h-25 w-50 mx-auto align-items-center">
-                <div className="spinner-border text-secondary mx-auto spinner-splash" role="status">
+            <div className="row align-items-center justify-content-center h-25 m-0">
+                <div className="spinner-border text-secondary spinner-splash" role="status">
                     <span className="sr-only"></span>
                 </div>
             </div>
-            <div className="row h-25 w-50 mx-auto align-items-center">
-                <img src={logoPicceTextual} className="mx-auto logo-picce-textual" alt="Logo textual Picce"></img>
+            <div className="row align-items-center justify-content-center h-25 w-50 px-2 m-0">
+                <img src={logoPicceTextual} className="logo-picce-textual" alt="Logo textual Picce"></img>
             </div>
             <ColoredBorder />
             <style>{styles}</style>
