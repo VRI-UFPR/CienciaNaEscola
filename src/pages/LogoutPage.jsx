9bafb79c76fdeb31a5f29d06575db3a2f1df3ac6
Yuri Junqueira Tobias <Yuri.r.tobias@hotmail.com.br>
2023-04-14 20:03:47 -0300

    [FIX] modifiquei algumas classes e importei o componente HelpButton

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import helpIcon from '../assets/images/helpIcon.svg';
 import NavBar from '../components/Navbar';
+import HelpButton from '../components/HelpButton';
 
 const styles = `
 
     .bg-grey {
         background-color: #D9D9D9;
+    }
+
+    .bg-dark-grey {
+        background-color: #787878;
+    }
+
+    .bg-red {
+        background-color: #EA636F;
     }
 
     .font-barlow {
@@ -22,86 +30,42 @@ const styles = `
         font-size: x-large;
     }
 
-    .container{
-        border-radius: 30px;
-        padding: 20px;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
+    .box {
         width: 80%;
-        height: 18%;
-    }
-
-    .center {
-        position: absolute;
-        top: 40%;
-        left: 10%;
-    }
-
-    .centerb {
-        position: absolute;
-        top: 57%;
-        left: 10%;     
-    }
-
-
-    .red-button {
+        max-width: 350px;
+        text-align: center;
         border-radius: 30px;
-        padding-left: 30px;
-        padding-right: 30px;
-        background-color: #EA636F;
+    }
+
+    .buttons {
         color: #FFF;
         font-weight: 700;
-        font-size: 130%;
-        font-family: 'Century Gothic', sans-serif;
+        font-size: 1.2rem;
         border: none;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);;
-    }
-    .grey-button {
         border-radius: 30px;
-        padding-left: 30px;
-        padding-right: 30px;
-        background-color: #787878;
-        color: #FFF;
-        font-weight: 700;
-        font-size: 130%;
-        margin-top: 10px;
-        font-family: 'Century Gothic', sans-serif;
-        border: none;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     }
 `;
 
-
-function LogoutPage(props){
+function LogoutPage(props) {
     return (
-        <div className="d-flex flex-column font-barlow vh-100 align-center justify-content-center">
+        <div className="d-flex flex-column font-barlow vh-100">
             <NavBar />
-            <div className="d-flex flex-column bg-grey container center">
-                <h1 className="font-century-gothic"> Tem certeza que deseja fazer logout? </h1>
-                <button className="grey-button"> Fazer Logout </button>
-            </div>
-            <div className="container centerb">
-                <button className="red-button"> Cancelar </button>
-            </div>
-            <div className="row d-flex flex-grow-1 align-items-end pt-0 pb-4 mx-0 px-2">
-                <div className="row justify-content-end px-0 mx-0">
-                        <div className="col-3 d-flex align-items-center justify-content-end px-0">
-                            <button
-                                type="button"
-                                style={{
-                                    maxWidth: '32px',
-                                }}
-                                className="btn rounded-circle bg-crimson h-auto w-100 p-1"
-                            >
-                                <img src={helpIcon} alt="Ícone" className="w-100"></img>
-                            </button>
-                        </div>
-                    </div>
+            <div className="d-flex flex-column justify-content-center flex-grow-1">
+                <div className="container box d-flex flex-column bg-grey px-4 py-3">
+                    <h1 className="font-century-gothic mb-3"> Tem certeza que deseja fazer logout? </h1>
+                    <button className="buttons font-century-gothic bg-dark-grey mx-4 py-1"> Fazer Logout </button>
                 </div>
+                <div className="container box p-3">
+                    <button className="buttons font-century-gothic bg-red px-5 py-1"> Cancelar </button>
+                </div>
+            </div>
+            <div className="d-flex justify-content-end pe-4 pb-4">
+                <HelpButton />
+            </div>
             <style>{styles}</style>
         </div>
-    )
+    );
 }
 
 export default LogoutPage;
