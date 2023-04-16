6387ec3484791d49b5bedfe51d9b6ab57b2a96f4
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-16 17:01:38 -0300

    [FIX] Correções comportamentais para helpButton

--- src/components/HelpButton.jsx
+++ src/components/HelpButton.jsx
@@ -3,13 +3,28 @@ import React from 'react';
 import helpIcon from '../assets/images/helpIcon.svg';
 
 const styles = `
-    .bg-crimson {
-        background-color: #EC6571;
-    }
-
     .help-button{
         min-height: 0px;
         line-height: 0px;
+    }
+
+    .btn-crimson {
+        color: #fff;
+        background-color: #EC6571;
+        border-color: #EC6571;
+    }
+
+    .btn-crimson:hover {
+        color: #fff;
+        background-color: #780f18;
+        border-color: #780f18;
+    }
+
+    .btn-crimson:focus {
+        color: #fff;
+        background-color: #780f18;
+        border-color: #780f18;
+        box-shadow: 0 0 0 0.25rem rgba(225, 83, 97, 0.5);
     }
 `;
 
@@ -21,7 +36,7 @@ function HelpButton(props) {
                 style={{
                     maxWidth: '32px',
                 }}
-                className="btn help-button bg-crimson rounded-circle h-auto w-100 p-1"
+                className="btn btn-crimson help-button rounded-circle h-auto w-100 p-1"
             >
                 <img src={helpIcon} alt="Ícone do botão de ajuda" className="w-100"></img>
             </button>
