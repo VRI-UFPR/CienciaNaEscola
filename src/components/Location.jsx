00d3a964db5679f80a7ca9dcdadf7bd663603288
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-17 08:43:47 -0300

    [FIX] Borda e padrão de código

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -38,10 +38,10 @@ const styles = `
 
 export function Location(props) {
     return (
-        <div className="rounded shadow bg-white overflow-hidden p-0">
+        <div className="rounded-4 shadow bg-white overflow-hidden p-0">
             <div className="row overflow-hidden m-0">
                 <div className="col-2 d-flex bg-pastel-blue p-0">
-                    <div class="location-icon ratio ratio-1x1 w-50 align-self-center mx-auto">
+                    <div class="location-icon ratio ratio-1x1 align-self-center w-50 mx-auto">
                         <img src={iconLocation} alt="" />
                     </div>
                 </div>
