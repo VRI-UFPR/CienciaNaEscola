85f2bfceb7f28a7c59e0b7bfe5b500770934c313
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-17 10:55:28 -0300

    [FIX] Dimensões máximas dinâmicas para Splash

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -5,16 +5,16 @@ import ColoredBorder from '../components/ColoredBorder';
 
 const styles = `
     .logo-picce-circular{
-        max-width: 350px;
+        max-width: 30%;
     }
 
     .logo-picce-textual{
-        max-width: 200px;
+        max-width: 40%;
     }
 
     .spinner-splash{
-        width: 50px;
-        height: 50px;
+        width: 45px;
+        height: 45px;
     }
 `;
 
@@ -23,14 +23,16 @@ function SplashPage(props) {
         <div className="d-flex flex-column align-items-center vh-100">
             <ColoredBorder />
             <div className="row flex-grow-1 align-items-end justify-content-center w-75 m-0">
-                <img src={logoPicceCircular} className="logo-picce-circular" alt="Logo gráfico Picce"></img>
+                <div className="ratio ratio-1x1 logo-picce-circular h-75">
+                    <img src={logoPicceCircular} className="w-100" alt="Logo gráfico Picce"></img>
+                </div>
             </div>
             <div className="row align-items-center justify-content-center h-25 m-0">
                 <div className="spinner-border text-secondary spinner-splash" role="status">
                     <span className="sr-only"></span>
                 </div>
             </div>
-            <div className="row align-items-center justify-content-center h-25 w-50 px-2 m-0">
+            <div className="row align-items-center justify-content-center h-25 px-2 m-0">
                 <img src={logoPicceTextual} className="logo-picce-textual" alt="Logo textual Picce"></img>
             </div>
             <ColoredBorder />
