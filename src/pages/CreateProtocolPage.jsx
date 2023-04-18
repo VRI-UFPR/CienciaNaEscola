e0a75a28fede9aab3f1592d7ae8c1a2ca037ab17
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-18 20:06:10 -0300

    [FIX] Correções de aspecto

--- src/pages/CreateProtocolPage.jsx
+++ src/pages/CreateProtocolPage.jsx
@@ -70,7 +70,7 @@ function CreateProtocolPage(props) {
                     <h1 className="font-century-gothic color-grey fs-3 fw-bold p-0 pb-4 pb-lg-5 m-0">Gerador de formulários</h1>
                 </div>
                 <div className="row flex-grow-1 m-0">
-                    <div className="col-12 col-lg-3 col-xl-2 p-0 pb-4">
+                    <div className="col-12 col-lg-auto p-0 pb-4">
                         <div className="bg-pastel-blue d-flex flex-column align-items-center rounded-4 p-4">
                             <h1 className="font-century-gothic fs-3 fw-bold text-white">Adicionar</h1>
                             <button
@@ -79,23 +79,23 @@ function CreateProtocolPage(props) {
                                 onClick={handleTextBoxAdd}
                             >
                                 <IconPlus className="icon-plus" />
-                                <span className="fs-5 fw-normal lh-1 ps-3">Caixa de texto</span>
+                                <span className="fs-5 fw-normal lh-1 ps-3 text-nowrap">Caixa de texto</span>
                             </button>
                             <button type="button" className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100">
                                 <IconPlus className="icon-plus" />
-                                <span className="fs-5 fw-normal lh-1 ps-3">Lista suspensa</span>
+                                <span className="fs-5 fw-normal lh-1 ps-3 text-nowrap">Lista suspensa</span>
                             </button>
                             <button type="button" className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100">
                                 <IconPlus className="icon-plus" />
-                                <span className="fs-5 fw-normal lh-1 ps-3">Múltipla escolha</span>
+                                <span className="fs-5 fw-normal lh-1 ps-3 text-nowrap">Seleção única</span>
                             </button>
                             <button type="button" className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100">
                                 <IconPlus className="icon-plus" />
-                                <span className="fs-5 fw-normal lh-1 ps-3">Múltipla escolha</span>
+                                <span className="fs-5 fw-normal lh-1 ps-3 text-nowrap">Múltipla escolha</span>
                             </button>
                             <button type="button" className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100">
                                 <IconPlus className="icon-plus" />
-                                <span className="fs-5 fw-normal lh-1 ps-2 text-truncate">Subformulário</span>
+                                <span className="fs-5 fw-normal lh-1 ps-3 text-nowrap">Subformulário</span>
                             </button>
                         </div>
                     </div>
