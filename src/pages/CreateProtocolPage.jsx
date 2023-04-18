4fba3fa39ba99f1b7cc5dc6cee6c17df6a1a922f
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-18 09:45:33 -0300

    [ADD] CreateProtocolPage preparado para caixas de texto dinâmicas

--- src/pages/CreateProtocolPage.jsx
+++ src/pages/CreateProtocolPage.jsx
@@ -3,6 +3,7 @@ import NavBar from '../components/Navbar';
 import { ReactComponent as IconPlus } from '../assets/images/iconPlus.svg';
 import TextButton from '../components/TextButton';
 import RoundedButton from '../components/RoundedButton';
+import CreateTextBoxInput from '../components/CreateTextBoxInput';
 
 const CreateProtocolStyles = `
     .font-barlow {
@@ -28,16 +29,36 @@ const CreateProtocolStyles = `
     .bg-pastel-blue {
         background-color: #91CAD6;
     }
+
+    .icon-plus {
+        min-width: 30px;
+    }
 `;
 
 function CreateProtocolPage(props) {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [inputs, setInputs] = useState([]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const protocol = { title, description };
+        const protocol = { title, description, inputs };
         console.log(JSON.stringify(protocol));
+    };
+
+    const handleTextBoxAdd = () => {
+        setInputs([...inputs, { question: '', description: '' }]);
+    };
+
+    const handleTextBoxRemove = (indexToRemove) => {
+        const updatedInputs = inputs.filter((_, index) => index !== indexToRemove);
+        setInputs(updatedInputs);
+    };
+
+    const handleTextBoxChange = (index, input) => {
+        const updateInputs = [...inputs];
+        updateInputs[index] = input;
+        setInputs(updateInputs);
     };
 
     return (
@@ -51,26 +72,30 @@ function CreateProtocolPage(props) {
                     <div className="col-12 col-lg-3 col-xl-2 p-0 pb-4">
                         <div className="bg-pastel-blue d-flex flex-column align-items-center rounded-4 p-4">
                             <h1 className="font-century-gothic fs-3 fw-bold text-white">Adicionar</h1>
-                            <div className="d-flex align-items-center w-100">
-                                <IconPlus width={30} style={{ minWidth: '30px' }} />
-                                <span className="fs-4 fw-normal ps-4">Caixa de texto</span>
-                            </div>
-                            <div className="d-flex align-items-center w-100">
-                                <IconPlus width={30} style={{ minWidth: '30px' }} />
-                                <span className="fs-4 fw-normal ps-4">Lista suspensa</span>
-                            </div>
-                            <div className="d-flex ps- w-100 align-items-center">
-                                <IconPlus width={30} style={{ minWidth: '30px' }} />
-                                <span className="fs-4 fw-normal ps-4">Seleção única</span>
-                            </div>
-                            <div className="d-flex align-items-center w-100">
-                                <IconPlus width={30} style={{ minWidth: '30px' }} />
-                                <span className="fs-4 fw-normal ps-4">Múltipla escolha</span>
-                            </div>
-                            <div className="d-flex align-items-center w-100">
-                                <IconPlus width={30} style={{ minWidth: '30px' }} />
-                                <span className="fs-4 fw-normal ps-4 text-truncate">Subformulário</span>
-                            </div>
+                            <button
+                                type="button"
+                                className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100"
+                                onClick={handleTextBoxAdd}
+                            >
+                                <IconPlus width={30} className="icon-plus" />
+                                <span className="fs-4 fw-normal lh-1 ps-4">Caixa de texto</span>
+                            </button>
+                            <button type="button" className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100">
+                                <IconPlus width={30} className="icon-plus" />
+                                <span className="fs-4 fw-normal lh-1 ps-4">Lista suspensa</span>
+                            </button>
+                            <button type="button" className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100">
+                                <IconPlus width={30} className="icon-plus" />
+                                <span className="fs-4 fw-normal lh-1 ps-4">Múltipla escolha</span>
+                            </button>
+                            <button type="button" className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100">
+                                <IconPlus width={30} className="icon-plus" />
+                                <span className="fs-4 fw-normal lh-1 ps-4">Múltipla escolha</span>
+                            </button>
+                            <button type="button" className="btn btn-transparent shadow-none m-0 p-0 mb-3 d-flex align-items-center w-100">
+                                <IconPlus width={30} className="icon-plus" />
+                                <span className="fs-4 fw-normal lh-1 ps-4 text-truncate">Subformulário</span>
+                            </button>
                         </div>
                     </div>
                     <div className="col d-flex flex-column p-0 ps-lg-5">
@@ -97,6 +122,15 @@ function CreateProtocolPage(props) {
                                     onChange={(event) => setDescription(event.target.value)}
                                 ></textarea>
                             </div>
+                            {inputs.map((input, index) => (
+                                <CreateTextBoxInput
+                                    key={index}
+                                    index={index}
+                                    inputState={input}
+                                    onTextBoxChange={handleTextBoxChange}
+                                    onTextBoxRemove={() => handleTextBoxRemove(index)}
+                                />
+                            ))}
                             <div className="row justify-content-between m-0">
                                 <div className="col-2"></div>
                                 <div className="col-8 col-lg-4">
