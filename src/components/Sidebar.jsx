68c64fa22330b4333e02aa55e729115654c52981
Yuri Junqueira Tobias <Yuri.r.tobias@hotmail.com.br>
2023-04-18 21:37:50 -0300

    [FIX] Alterações em Sidebar de acordo com o novo design

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import BuscarImg from '../assets/images/BuscarImg.svg';
+import ExitIcon from '../assets/images/ExitSidebarIcon.svg';
 import PerfilImg from '../assets/images/PerfilImg.png';
 
 const styles = `
@@ -11,7 +11,16 @@ const styles = `
         font-family: 'Century Gothic', sans-serif;
     }
 
-    .sidebar-wrapper img {
+    .bg-pink {
+        background-color: #F59489;
+    }
+
+    .exit-image {
+        max-width: 12px;
+        max-height: 12px;
+    }
+
+    .profile-image {
         width: 110px;
         height: 110px;
         border: 8px solid #AAD390;
@@ -22,17 +31,28 @@ const styles = `
         opacity: 0.8;
     }
 
-    .sidebar-wrapper a:hover {
-        background-color: #FFBCB5;
+    .sidebar-wrapper .btn:active {
+        background-color: #F59489;
     }
 
+    .sidebar-wrapper .btn:hover, 
+    .sidebar-wrapper a:hover, 
+    .sidebar-wrapper .btn:active, 
+    .sidebar-wrapper a:active {
+        background-color: #FFBCB5;
+    }
 `;
 
 function Sidebar(props) {
     return (
-        <div className="sidebar-wrapper d-flex flex-column">
-            <div className="container d-flex flex-column align-items-center">
-                <img className="rounded-circle my-4" src={PerfilImg} alt="Perfil" />
+        <div className="sidebar-wrapper d-flex flex-column flex-grow-1">
+            <div className="container d-flex d-lg-none justify-content-end p-0">
+                <button type="button" className="btn btn-primary bg-pink rounded-circle border-0" data-bs-dismiss="offcanvas">
+                    <img className="exit-image" src={ExitIcon} alt="Exit Sidebar Icon" />
+                </button>
+            </div>
+            <div className="container d-flex flex-column align-items-center pt-3 pb-4">
+                <img className="profile-image rounded-circle" src={PerfilImg} alt="Perfil" />
             </div>
             <div className="container d-flex flex-column font-barlow fw-medium p-0 pb-4">
                 <h1 className="text-start text-white font-century-gothic fw-bold fs-2 mb-0 ps-4 pb-3">Menu</h1>
@@ -53,6 +73,13 @@ function Sidebar(props) {
                 </a>
                 <a className="text-white text-decoration-none ps-5 py-2" href="/">
                     Logout
+                </a>
+            </div>
+            <div className="container d-flex flex-column flex-grow-1 justify-content-end font-barlow text-white p-0 pb-4">
+                <h3 className="fw-bold fs-6 ps-4 ps-md-5">Denúncias</h3>
+                <span className="fw-medium ps-4 ps-md-5">Acesse o site</span>
+                <a href="/" className="text-white text-decoration-underline fw-medium pb-2 ps-4 ps-md-5">
+                    www.denuncias.com
                 </a>
             </div>
             <style>{styles}</style>
