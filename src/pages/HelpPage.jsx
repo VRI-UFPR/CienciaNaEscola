8a4811c35b871ecf715cb3b682e30dfe47391c4b
YuriJT <Yuri.r.tobias@hotmail.com.br>
2023-04-24 09:03:16 -0300

    [FIX] apenas 'convenções'

--- src/pages/HelpPage.jsx
+++ src/pages/HelpPage.jsx
@@ -1,18 +1,16 @@
-import React from "react";
-import NavBar from "../components/Navbar";
+import React from 'react';
+import NavBar from '../components/Navbar';
 
 const styles = `
-    .tab div {
-        display: none;
+    .color-lc-pink {
+        color: rgba(236, 101, 113, 1);
     }
 
-    .tab div:target {
-        display: block; 
+    .color-eerie-black {
+        color: rgba(38, 38, 38, 0.56);
     }
 
-    .search-input {
-        width: 100%;
-        border: 0px;
+    .bg-gainsbor {
         background-color: rgba(217, 217, 217, 1);
     }
 
@@ -24,59 +22,61 @@ const styles = `
         font-family: 'Barlow', sans-serif;
     }
 
+    .tab div {
+        display: none;
+    }
+
+    .tab div:target {
+        display: block; 
+    }
+
     ::placeholder {
         color: #FFFFFF;
         font-family: 'Barlow', sans-serif;
     }
-
-    h2 {
-        color: rgba(236, 101, 113, 1);
-        font-weight: 700;
-    }
-
-    h5 {
-        font-weight: 500;
-    }
-
-    a {
-        color: rgba(38, 38, 38, 0.56);
-        font-family: 'Barlow', sans-serif;
-        font-weight: 500;
-    }    
 `;
 
 function HelpPage(props) {
-    return ( 
-        <div>
+    return (
+        <div className="d-flex flex-column flex-grow-1">
             <NavBar />
-            <div className="d-flex flex-column mx-4">
-                <div>
-                    <h2 className="font-century-gothic mb-3 mt-3 ms-1">Precisa de ajuda?</h2>
+            <div className="d-flex flex-column flex-grow-1 px-4">
+                <div className="row py-3 m-0">
+                    <h2 className="font-century-gothic fw-bold color-lc-pink p-0 m-0">Precisa de ajuda?</h2>
                 </div>
-                <div>
-                    <input className="search-input shadow-sm rounded-pill px-3 mb-3" type="search" placeholder="Qual é sua dúvida?" />
+                <div className="row pb-3 m-0">
+                    <input
+                        className="shadow-sm border-0 rounded-pill bg-gainsbor py-1 w-100"
+                        type="search"
+                        placeholder="Qual é sua dúvida?"
+                    />
                 </div>
-                <div>
-                    <h5 className="font-barlow mb-3 ms-1">Dúvidas frequentes: </h5>
+                <div className="row pb-3 m-0">
+                    <h5 className="font-barlow fw-medium fs-6 pb-3 px-0 m-0">Dúvidas frequentes: </h5>
                     <div className="tab d-flex flex-column">
-                        <a className="mb-3" href="#link1">-Lorem ipsum dolor sit amet, consec?</a>
+                        <a className="font-barlow fw-medium color-eerie-black pb-3" href="#link1">
+                            -Lorem ipsum dolor sit amet, consec?
+                        </a>
                         <div id="link1">
                             <p>Ola mundo</p>
                         </div>
-                        <a className="mb-3" href="#link2">-Lorem ipsum dolor sit amet, consec?</a>
+                        <a className="font-barlow fw-medium color-eerie-black pb-3" href="#link2">
+                            -Lorem ipsum dolor sit amet, consec?
+                        </a>
                         <div id="link2">
                             <p>Isso eh</p>
                         </div>
-                        <a className="mb-3" href="#link3">-Lorem ipsum dolor sit amet, consec?</a>
+                        <a className="font-barlow fw-medium color-eerie-black pb-3" href="#link3">
+                            -Lorem ipsum dolor sit amet, consec?
+                        </a>
                         <div id="link3">
                             <p>Um teste</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <style dangerouslySetInnerHTML={{ __html: styles }} />
+            <style>{styles}</style>
         </div>
-        
     );
 }
 
