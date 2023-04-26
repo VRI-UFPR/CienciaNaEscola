8d4549ae22f2f72ba4195ed17f216f0da4e64b9a
eduardaafreitas <eduardaaguiar1012@hotmail.com>
2023-04-26 17:51:15 -0300

    [ADD] começo da issue

---
+++ src/components/SubForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const styles = `
+    .font-barlow {
+        font-family: 'Barlow', sans-serif;
+    }
+    .font-century-gothic {
+        font-family: 'Century Gothic', sans-serif;
+    }
+
+    .bg-pastel-blue {
+        background-color: #91CAD6;
+    }
+
+`;
+
+function SubForm(props) {
+    return (
+
+    );
+}
+
+export default SubForm;
