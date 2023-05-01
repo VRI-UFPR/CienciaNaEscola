299dc26b2751204421140b33833f24e9a0a6438d
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-04-30 22:52:24 -0300

    [FIX] Alinhamento de botão inferior

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -177,11 +177,11 @@ function SignUpPage(props) {
                 </div>
 
                 <div className="row justify-content-between w-100 mt-5 mx-0">
-                    <div className="col-auto"></div>
+                    <div className="col-1"></div>
                     <div className="col-auto align-items-center p-0">
                         <TextButton className="px-5" hsl={[97, 43, 70]} text="Cadastre-se" onClick={signUpHandler} />
                     </div>
-                    <div className="col-2 d-flex align-items-end justify-content-end p-0">
+                    <div className="col-1 d-flex align-items-end justify-content-end p-0">
                         <RoundedButton />
                     </div>
                 </div>
