d32b388c88fa85854fbc5c6820a91e264ee8cc12
Juliana Zambon <zambon.juju@gmail.com>
2023-05-01 22:57:32 -0300

    [

--- src/components/InputOptions.jsx
+++ src/components/InputOptions.jsx
@@ -1,62 +1,129 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
+import iconFile from '../assets/images/iconFile.svg';
+import iconTrash from '../assets/images/iconTrash.svg';
+
+import RoundedButton from './RoundedButton';
 
 const styles = `
-    
-    h2 {
-        color: #91CAD6;
+    .font-century-gothic {
+        font-family: 'Century Gothic', sans-serif;
+    }    
+
+    .font-barlow {
+        font-family: 'Barlow', sans-serif;
     }
 
-    .quest {
-        background-color: #D3D3D3;
-        border-radius: 10px;
+    .bg-light-grey{
+        background-color: #D9D9D9;
     }
 
-    p {
-        color: red;
+    .text-steel-blue {
+        color: #4E9BB9;
     }
 
-    .form {
-        display: flex;
-        flex-direction: column;
-        padding: 1px;
-    }
-
-    .error {
-        color: red;
+    .border-steel-blue{
+        border-color: #4E9BB9 !important;
     }
 
     `;
 
-function InputOptions (props){
+function InputOptions(props) {
+    const { index, inputState, onTextBoxChange, onTextBoxRemove } = props;
 
+    const handleTextBoxChange = (event, field) => {
+        const updatedTextBox = { ...inputState };
+        updatedTextBox[field] = event.target.value;
+        onTextBoxChange(index, updatedTextBox);
+    };
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     watch,
+    //     formState: { errors },
+    // } = useForm();
 
-  const [questionInputText, setQuestionInputText] = useState('')
-  const [descriptionInputText, setDescriptionInputText] = useState('')
+    // const [questionInputText, setQuestionInputText] = useState('');
+    // const [descriptionInputText, setDescriptionInputText] = useState('');
 
-  const onSubmit = data => console.log(data)
-
+    // const onSubmit = (data) => console.log(data);
 
     return (
-        <div className="row buttons-row m-0">
-            <div className="col d-flex align-items-start ps-1 p-0">
-                <h2>Seleção Única</h2>
+        <div className="pb-4 pb-lg-5">
+            <div className="row justify-content-between m-0">
+                <div className="col d-flex justify-content-start p-0">
+                    <h1 className="font-century-gothic text-steel-blue fs-3 fw-bold p-0 pb-4 m-0">Seleção Única</h1>
+                </div>
+                <div className="col d-flex justify-content-end p-0">
+                    <RoundedButton hsl={[190, 46, 70]} icon={iconFile} />
+                    <RoundedButton className="ms-2" hsl={[190, 46, 70]} icon={iconTrash} onClick={onTextBoxRemove} />
+                </div>
             </div>
-            <div className="quest p-5">
-            <form  onSubmit={handleSubmit(onSubmit)}>
-                <div className='form'>
-                    <label>Pergunta</label>
- <input {...register("question", { required: true})} />
-      {errors.question && <span className='error'>*Este campo é obrigatório</span>}
-      
+            <div className="bg-light-grey rounded-4 lh-1 w-100 p-4">
+                <div className="mb-3">
+                    <label htmlFor="question" className="form-label fs-5 fw-medium">
+                        Pergunta
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control bg-transparent border-0 border-bottom border-steel-blue rounded-0 fs-5 lh-1 p-0"
+                        id="question"
+                        aria-describedby="questionHelp"
+                        onChange={(event) => handleTextBoxChange(event, 'question')}
+                    />
+                    <div id="questionHelp" className="form-text text-danger fs-6 fw-medium">
+                        *Este campo é obrigatório.
+                    </div>
+                </div>
+                <div className="pb-1">
+                    <label htmlFor="description" className="form-label fs-5 fw-medium">
+                        Descrição
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control bg-transparent border-0 border-bottom border-steel-blue rounded-0 fs-5 lh-1 p-0"
+                        id="description"
+                        onChange={(event) => handleTextBoxChange(event, 'description')}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="option0" className="form-label fs-5 fw-medium">
+                        Opção 0
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control bg-transparent border-0 border-bottom border-steel-blue rounded-0 fs-5 lh-1 p-0"
+                        id="option0"
+                        aria-describedby="questionHelp"
+                        onChange={(event) => handleTextBoxChange(event, 'question')}
+                    />
+                    <div id="questionHelp" className="form-text text-danger fs-6 fw-medium">
+                        *Por favor, preencha esta opção
+                    </div>
+                </div>
+                <div id="questionHelp" className="form-text text-danger fs-6 fw-medium">
+                    O campo precisa ter pelo menos duas opções!
+                </div>
+            </div>
+            <div class="form-check form-switch d-flex justify-content-end p-1">
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                <label class="form-check-label font-barlow fw-medium ps-1" for="flexSwitchCheckDefault">
+                    Obrigatório
+                </label>
+            </div>
+            {/* <div className="quest p-5">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form">
+                        <label>Pergunta</label>
+                        <input {...register('question', { required: true })} />
+                        {errors.question && <span className="error">*Este campo é obrigatório</span>}
 
-      <label>Descrição</label>
-      <input  {...register("description")} />    
-      <input type="submit" /></div>
-    </form>
-            </div>
+                        <label>Descrição</label>
+                        <input {...register('description')} />
+                        <input type="submit" />
+                    </div>
+                </form>
+            </div> */}
             <style>{styles}</style>
         </div>
     );
