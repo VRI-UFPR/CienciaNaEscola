1f0045757baa1b29b5f29e09f05b4b7c47af5f4f
eduardaafreitas <eduardaaguiar1012@hotmail.com>
2023-05-02 00:30:37 -0300

    [ADD] classes organizadas e esqueleto feito

--- src/components/SubForm.jsx
+++ src/components/SubForm.jsx
@@ -1,21 +1,37 @@
+import { getByTitle } from '@testing-library/react';
 import React from 'react';
 
 const styles = `
+    .font-century-gothic {
+        font-family: 'Century Gothic', sans-serif;
+    }    
+        
     .font-barlow {
         font-family: 'Barlow', sans-serif;
     }
-    .font-century-gothic {
-        font-family: 'Century Gothic', sans-serif;
+
+    .bg-light-grey{
+        background-color: #D9D9D9;
     }
 
-    .bg-pastel-blue {
-        background-color: #91CAD6;
+    .text-steel-blue {
+        color: #4E9BB9;
     }
 
+    .border-steel-blue{
+        border-color: #4E9BB9 !important;
+    }
 `;
 
 function SubForm(props) {
     return (
+        <div className=""> //classe background
+            <div className="" > //titulo SubForm
+            </title >
+                //titulo aqui
+            </title >
+            </div >
+        </div >
 
     );
 }
