f99d88125ca1abf6250020f111efd429c89f474e
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-05-08 13:21:19 -0300

    [FIX] Dia na data pré-preenchida

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -34,7 +34,7 @@ const styles = `
 function DateInput(props) {
     const currentDate = () => {
         const date = new Date();
-        const day = String(date.getDay()).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const year = String(date.getFullYear());
         return year + '-' + month + '-' + day;
