88a0d1cad511c3dfd38b1180c436fc7d6596d4df
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-05-15 11:15:46 -0300

    [TEST (deploy)]: HashRouter reinserido

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
+import { HashRouter } from 'react-router-dom';
 
 const styles = ``;
 
 function App(props) {
     return (
         <AuthProvider>
-            <AppRoutes />
+            <HashRouter>
+                <AppRoutes />
+            </HashRouter>
             <style> {styles} </style>
         </AuthProvider>
     );
