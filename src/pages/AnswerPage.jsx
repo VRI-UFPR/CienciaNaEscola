798220d4eea060f27850904c840dd5ccb492150f
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-05-22 13:07:54 -0300

    #139 [REFACTOR]: Repetição de código e condicionais indevidos

--- src/pages/AnswerPage.jsx
+++ src/pages/AnswerPage.jsx
@@ -50,6 +50,22 @@ function AnswerPage(props) {
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
 
+    const formatAnswer = (input, inputAnswers) => {
+        switch (input.type) {
+            case 0:
+                return inputAnswers[0].value;
+            case 1:
+            case 2:
+                return inputAnswers
+                    .filter((option) => option.value === 'true')
+                    .map((option) => input.sugestions[option.placement].value)
+                    .join(', ');
+
+            default:
+                break;
+        }
+    };
+
     useEffect(() => {
         if (user.token) {
             axios
@@ -79,8 +95,8 @@ function AnswerPage(props) {
             <div className="d-flex flex-column flex-grow-1 bg-white p-4 p-lg-5">
                 <div className="row m-0 p-0 mb-4">
                     <h1 className="color-dark-gray w-auto fw-bold fs-4 m-0 p-0">
-                        <Link className="color-dark-gray" to={`../../protocol/${id}`}>
-                            {protocolAnswer.length > 0 ? protocolAnswer[0].form.title : 'Protocolo inválido. Sem respostas'}
+                        <Link className="color-dark-gray" to={`/protocol/${id}`}>
+                            {protocolAnswer.length > 0 ? protocolAnswer[0].form.title : 'Inválido'}
                         </Link>{' '}
                         -
                     </h1>
@@ -106,7 +122,7 @@ function AnswerPage(props) {
                     <h2 className="color-dark-gray fw-medium fs-5 m-0 mb-3">Quem respondeu?</h2>
                     {protocolAnswer.map((answer, answerIndex) => {
                         return (
-                            <div key={answer.id} className="bg-white rounded-4 p-2 px-3 mb-3">
+                            <div key={'Person ' + answer.id} className="bg-white rounded-4 p-2 px-3 mb-3">
                                 <p className="fw-medium fs-6 m-0">
                                     <a
                                         href="#answerTab"
@@ -123,108 +139,41 @@ function AnswerPage(props) {
                     })}
                 </div>
 
-                {/* <div className="bg-light-gray rounded-4 p-3 mb-3">
-                    <h2 className="color-dark-gray fw-medium fs-5 m-0 mb-3">Visualizar</h2>
-                    <select className="form-select fw-medium fs-6 m-0 bg-white rounded-4 p-2 px-3" aria-label="Default select example">
-                        <option className="color-dark-gray fw-medium fs-5 m-0" value="1">
-                            Todas as questões
-                        </option>
-                        <option className="color-dark-gray fw-medium fs-5 m-0" value="2">
-                            Questão 1
-                        </option>
-                        <option className="color-dark-gray fw-medium fs-5 m-0" value="3">
-                            Questão 2...
-                        </option>
-                    </select>
-                </div> */}
-
                 <div id="answerTab" className="mb-3 mb-lg-4">
-                    {protocolAnswer.length > 0 ? (
-                        protocolAnswer[0].form.inputs.map((input, inputIndex) => {
-                            if (selectedQuestion === undefined || selectedQuestion === inputIndex) {
-                                switch (input.type) {
-                                    case 0:
-                                        return (
-                                            <div key={input.id} className="bg-light-gray rounded-4 p-3 pb-1 mb-3">
-                                                <a
-                                                    href="#answerTab"
-                                                    onClick={() => {
-                                                        setSelectedPerson(undefined);
-                                                        setSelectedQuestion(inputIndex);
-                                                    }}
-                                                    className="fw-medium fs-5 m-0 mb-3"
-                                                >
-                                                    Pergunta {input.placement}
-                                                </a>
-                                                <h3 className="color-dark-gray fw-bold fs-6 m-0 mb-3">{input.question}</h3>
-                                                {protocolAnswer.map((answer, answerIndex) => {
-                                                    if (selectedPerson === undefined || selectedPerson === answerIndex) {
-                                                        return (
-                                                            <div key={answer.id} className="bg-white rounded-4 p-2 px-3 mb-3">
-                                                                <p className="fw-medium fs-6 m-0">
-                                                                    Pessoa {answerIndex} -{' '}
-                                                                    <span className="color-dark-gray">
-                                                                        {answer.inputAnswers[input.id][0].value}
-                                                                    </span>
-                                                                </p>
-                                                            </div>
-                                                        );
-                                                    } else {
-                                                        return undefined;
-                                                    }
-                                                })}
-                                            </div>
-                                        );
-                                    case 1:
-                                    case 2:
-                                        return (
-                                            <div key={input.id} className="bg-light-gray rounded-4 p-3 pb-1 mb-3">
-                                                <a
-                                                    href="#answerTab"
-                                                    onClick={() => {
-                                                        setSelectedPerson(undefined);
-                                                        setSelectedQuestion(inputIndex);
-                                                    }}
-                                                    className="fw-medium fs-5 m-0 mb-3"
-                                                >
-                                                    Pergunta {input.placement}
-                                                </a>
-                                                <h3 className="color-dark-gray fw-bold fs-6 m-0 mb-3">{input.question}</h3>
-                                                {protocolAnswer.map((answer, answerIndex) => {
-                                                    if (selectedPerson === undefined || selectedPerson === answerIndex) {
-                                                        return answer.inputAnswers[input.id].map((option, optionIndex) => {
-                                                            if (option.value === 'true') {
-                                                                return (
-                                                                    <div key={option.id} className="bg-white rounded-4 p-2 px-3 mb-3">
-                                                                        <p className="fw-medium fs-6 m-0">
-                                                                            Pessoa {answerIndex} -{' '}
-                                                                            <span className="color-dark-gray">
-                                                                                {input.sugestions[optionIndex].value}
-                                                                            </span>
-                                                                        </p>
-                                                                    </div>
-                                                                );
-                                                            } else {
-                                                                return undefined;
-                                                            }
-                                                        });
-                                                    } else {
-                                                        return undefined;
-                                                    }
-                                                })}
-                                            </div>
-                                        );
-
-                                    default:
-                                        return undefined;
-                                }
-                            } else {
-                                return undefined;
-                            }
-                        })
-                    ) : (
-                        <p>Sem respostas</p>
-                    )}
+                    {protocolAnswer.length > 0 &&
+                        protocolAnswer[0].form.inputs
+                            .filter((_, inputIndex) => selectedQuestion === undefined || selectedQuestion === inputIndex)
+                            .map((input, inputIndex) => {
+                                return (
+                                    <div key={'Input ' + input.id} className="bg-light-gray rounded-4 p-3 pb-1 mb-3">
+                                        <a
+                                            href="#answerTab"
+                                            onClick={() => {
+                                                setSelectedPerson(undefined);
+                                                setSelectedQuestion(inputIndex);
+                                            }}
+                                            className="fw-medium fs-5 m-0 mb-3"
+                                        >
+                                            Pergunta {input.placement}
+                                        </a>
+                                        <h3 className="color-dark-gray fw-bold fs-6 m-0 mb-3">{input.question}</h3>
+                                        {protocolAnswer
+                                            .filter((answer, answerIndex) => selectedPerson === undefined || selectedPerson === answerIndex)
+                                            .map((answer, answerIndex) => {
+                                                return (
+                                                    <div key={'Answer ' + answer.id} className="bg-white rounded-4 p-2 px-3 mb-3">
+                                                        <p className="fw-medium fs-6 m-0">
+                                                            Pessoa {answerIndex} -{' '}
+                                                            <span className="color-dark-gray">
+                                                                {formatAnswer(input, answer.inputAnswers[input.id])}
+                                                            </span>
+                                                        </p>
+                                                    </div>
+                                                );
+                                            })}
+                                    </div>
+                                );
+                            })}
                 </div>
                 <div className="row justify-content-end mx-0">
                     <div className="col-2 d-flex align-items-end justify-content-end p-0">
