eb82ccd8f75e7cb49144e94ec8e0050dff03c90a
Eduardo Mathias <ems19@inf.ufpr.br>
2023-05-26 09:56:57 -0300

    [ERROR] Doesn't upload neither creates path'

--- form-creator-back/form-creator-api/src/api/middlewares/uploadHandler.ts
+++ form-creator-back/form-creator-api/src/api/middlewares/uploadHandler.ts
@@ -1,11 +1,17 @@
 import express = require('express');
 import multer = require('multer');
+import fs = require('fs');
 
 // cria uma instância do middleware configurada
 // destination: lida com o destino
 // filenane: permite definir o nome do arquivo gravado
 const storage = multer.diskStorage({
-    destination: "./uploads",
+    destination: (req, file, cb) => {
+        const path = `./form-creator-back/form-creator-api/uploads/gallery/`
+        if (!fs.existsSync(path))
+            fs.mkdirSync(path, { recursive: true })
+        return cb(null, path)
+      },
     // destino do arquivo no servidor
     // req: informações sobre a requisição feita pelo usuário
     // file: arquivo enviado
