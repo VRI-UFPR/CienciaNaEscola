68647c0a68b5417acf69e2a50a5908561c7f6ea0
YuriJT <Yuri.r.tobias@hotmail.com.br>
2023-06-18 22:41:23 -0300

    fix: retirada de console.log e correções na responsividade da sidebar

--- src/pages/HelpPage.jsx
+++ src/pages/HelpPage.jsx
@@ -34,18 +34,10 @@ const styles = `
     ::placeholder {
         color: #FFFFFF;
     }
-
-    @media (min-width: 992px) and (max-width: 1160px) {
-        .sidebar-col {
-            width: 25% !important;
-        }
-    }
 `;
 
 function toggle(id) {
-    console.log(id);
     var element = document.getElementById(id);
-    console.log(element);
     if (element) {
         var display = element.style.display;
     
@@ -70,7 +62,7 @@ function HelpPage(props) {
     return (
         <div className="d-flex flex-column vh-100">
             <div className="row flex-grow-1 m-0">
-                <div className={`sidebar-col col-md-3 col-lg-2 bg-coral-red d-none p-0 ${showSidebar ? 'd-lg-flex' : ''}`}>
+                <div className={`col-auto bg-coral-red d-none p-0 ${showSidebar ? 'd-lg-flex' : ''}`}>
                     <Sidebar modalRef={modalRef} />
                 </div>
                 <div className="col p-0">
