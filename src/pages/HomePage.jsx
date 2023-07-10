b840eafa3d8d83f19f85feebff403c45078c9075
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-07-10 15:40:10 -0300

    [TEST] Acesso a protocolos dividido entre usuários

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -57,7 +57,11 @@ function HomePage(props) {
             // .get(`http://localhost:3333/user/list/${user.id}`)
             // .get(`https://genforms.c3sl.ufpr.br/api/user/list/83`)
             axios
-                .get('https://run.mocky.io/v3/7eff7e79-0807-4e23-9aaa-59d4b5b9e176')
+                .get(
+                    `https://run.mocky.io/v3/${
+                        user.id === 89 ? 'dde59a84-a3f0-485c-9a12-1131685f8da3' : '888f413c-11ca-4be8-b5ef-f43b04bcbfd2'
+                    }`
+                )
                 .then((response) => {
                     setUserForms(response.data);
                     setIsLoading(false);
