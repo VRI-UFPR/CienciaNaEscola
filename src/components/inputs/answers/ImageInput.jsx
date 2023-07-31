7d7f40eee758930f8619280363f7a990478be74f
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-07-31 13:02:39 -0300

    #170 [FEAT]: Componente ImageInput para upload de imagens

---
+++ src/components/inputs/answers/ImageInput.jsx
@@ -0,0 +1,50 @@
+import { React, useEffect, useState } from 'react';
+
+const styles = `
+    .color-dark-gray {
+        color: #535353;
+    }
+
+    .font-barlow {
+        font-family: 'Barlow', sans-serif;
+    }
+
+    #submitbutton {
+        
+    }
+`;
+
+function ImageInput(props) {
+    const [image, setImage] = useState(['']);
+    const { onAnswerChange, input, answer } = props;
+
+    useEffect(() => {
+        onAnswerChange(input.id, image);
+    }, [image, input.id, onAnswerChange]);
+
+    return (
+        <div className="rounded-4 shadow bg-white w-100 p-3">
+            <form className="d-flex flex-column flex-grow-1">
+                <div className="row rounded p-0 pb-3 m-0">
+                    <label htmlFor="imageinput" className="control-label color-dark-gray font-barlow fw-medium fs-6 p-0 pb-3">
+                        {input.question}
+                    </label>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        name="imageinput"
+                        capture="environment"
+                        id="imageinput"
+                        placeholder="Adicione uma imagem"
+                        className="form-control rounded-0 shadow-none font-barlow fw-medium p-0"
+                        onChange={(e) => setImage([e.target.files[0]])}
+                        disabled={answer !== undefined}
+                    />
+                </div>
+            </form>
+            <style>{styles}</style>
+        </div>
+    );
+}
+
+export default ImageInput;
