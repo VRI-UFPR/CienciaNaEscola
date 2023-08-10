56b4ef0bfb2984eb3ce45e402a75681180f8ca1c
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-08-09 22:37:21 -0300

    #182 [FEAT]: Componente InfoGerais não interativo

--- src/components/inputs/answers/InfoGerais.jsx
+++ src/components/inputs/answers/InfoGerais.jsx
@@ -1,49 +1,34 @@
-import { React, useEffect, useState } from 'react';
+import { React } from 'react';
 
 const styles = `
+    .font-barlow {
+        font-family: 'Barlow', sans-serif;
+    }
+
     .color-dark-gray {
         color: #535353;
     }
 
-    .bloco-info-gerais {
-        border-top: 12px solid #F59489;
-    }
+    .color-gray {
+        color: #787878;
+    }   
 
-    #infogerais {
-        background: transparent;
-        border: none;
-        border-bottom: 1px solid #787878;
-        -webkit-box-shadow: none;
+    .bg-coral-red{
+        background-color: #F59489;
     }
 `;
 
 function InfoGerais(props) {
-    const [infos, setInfos] = useState(['']);
-    const { onAnswerChange, input, answer } = props;
-
-    useEffect(() => {
-        onAnswerChange(input.id, infos);
-    }, [infos, input.id, onAnswerChange]);
+    const { info } = props;
 
     return (
-        <div className="bloco-info-gerais rounded-4 shadow bg-white w-100">
-            <form className="d-flex flex-column flex-grow-1">
-                <div className="row rounded m-0 p-0 pb-3">
-                    <label htmlFor="infogerais" className="control-label color-dark-gray fw-bold fs-5 lh-lg p-0">
-                        Informações gerais:
-                    </label>
-                    <input
-                        type="text"
-                        name="infogerais"
-                        className="form-control rounded-0 shadow-none p-0"
-                        id="infogerais"
-                        placeholder="Adicionar descrição"
-                        onChange={(e) => setInfos([e.target.value])}
-                        value={answer ? answer[0].value : infos}
-                        disabled={answer !== undefined}
-                    />
-                </div>
-            </form>
+        <div className="rounded-4 shadow bg-white overflow-hidden w-100 p-0">
+            <div className="w-100 pb-3 bg-coral-red"></div>
+            <div className="p-3 pt-2">
+                <h1 className="color-dark-gray font-barlow text-break fw-bold fs-5 m-0 p-0 pb-2">Informações gerais:</h1>
+                <p className="color-gray font-barlow text-break fw-medium fs-6 m-0 p-0">{info}</p>
+            </div>
+
             <style>{styles}</style>
         </div>
     );
