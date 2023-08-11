60dc9596c2256241d149e92a32f940a8c910dd42
j-armenio <joaoarmeniosilveira@gmail.com>
2023-08-10 21:22:04 -0300

    #126 [FIX]: Lista suspensa terminada, com duas opções, ver com o Yuri qual é melhor.

--- src/components/inputs/answers/SelectInput.jsx
+++ src/components/inputs/answers/SelectInput.jsx
@@ -10,26 +10,42 @@ function SelectInput(props) {
     console.log(props);
     const options = props.input.options || [];
     return (
-        <div className="btn-group d-flex bg-white rounded">
-            <button className="btn col-11 text-start px-0" type="button">
-                {props.input.question}
-            </button>
-            <button
-                type="button"
-                className="btn btn-sm dropdown-toggle dropdown-toggle-split"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-            >
-                <span className="visually-hidden">Toggle Dropdown</span>
-            </button>
-            <ul className="dropdown-menu">
-                {options.map((option, index) => (
-                    <li key={index} value={option.value}>
-                        {option.label}
-                    </li>
-                ))}
-            </ul>
+        <div className="rounded shadow bg-white font-barlow pt-3 px-0 mt-3">
+            <div className="d-flex column justify-content-between m-0">
+                <div className="col-11 text-start px-3 pb-3">
+                    {props.input.question}
+                </div>
 
+                {/* <button
+                    type="button"
+                    className="btn btn-sm dropdown-toggle dropdown-toggle-split"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                    <span className="visually-hidden">Toggle Dropdown</span>
+                </button>
+                <ul className="dropdown-menu">
+                    {options.map((option, index) => (
+                        <li key={index} value={option.value}>
+                            {option.label}
+                        </li>
+                    ))}
+                </ul> */}
+
+                <div className="px-0 pt-2">
+                    <select className="form-select border-0 rounded-3">
+                        <option defaultValue=""></option>
+                        {options.map((option, index) => (
+                            <option key={index} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+
+            </div>
+            
             <style>{styles}</style>
         </div>
     );
