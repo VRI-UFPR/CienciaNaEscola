44c7b30107ccaa24b4e2024d784921f6b98caae6
Bakuretsy <joaoarmeniosilveira@gmail.com>
2023-08-15 21:13:32 -0300

    #126 [FIX]: Corrigindo o que foi pedido no CR

--- src/components/inputs/answers/SelectInput.jsx
+++ src/components/inputs/answers/SelectInput.jsx
@@ -7,15 +7,14 @@ const styles = `
 `;
 
 function SelectInput(props) {
-    console.log(props);
     const options = props.input.options || [];
     return (
         <div className="rounded shadow bg-white font-barlow pt-3 px-0 mt-3">
-            <div className="d-flex column align-items-center justify-content-between m-0 pb-3">
+            <div className="row align-items-center justify-content-between m-0 pb-3">
                 <div className="d-flex col-9 align-items-center text-break text-start px-3">
                     {props.input.question}
                 </div>
-                <div className="px-0">
+                <div className="col px-0">
                     <select className="form-select border-0 rounded-3">
                         <option defaultValue=""></option>
                         {options.map((option, index) => (
