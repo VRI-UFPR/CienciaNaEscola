cab3a4513608a58303f7d94348193c0f3c1e4feb
Juliana Zambon <zambon.juju@gmail.com>
2023-09-17 19:45:15 -0300

    [FIX] correção dos nomes dos membros na parte inferior da página

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -45,6 +45,20 @@ export const defaultNewInput = (type) => {
     };
 };
 
+export const aboutPICCE = (
+    <>
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute
+        iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+        tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit
+        laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem
+        ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrum exercitationem ullam corporis.
+    </>
+);
+
 export const teamMembers = (
     <>
         <p className="mb-4">Clara Drimel</p>
@@ -52,10 +66,12 @@ export const teamMembers = (
         <p className="mb-4">Eduarda de Aguiar Freitas</p>
         <p className="mb-4">Eduardo Mathias de Souza</p>
         <p className="mb-4">Eloisa Nielsen</p>
+        <p className="mb-4">Guilherme Stonoga Tedardi</p>
         <p className="mb-4">Izalorran Bonaldi</p>
         <p className="mb-4">João Armênio</p>
         <p className="mb-4">Juliana Zambon</p>
         <p className="mb-4">Matheus Piovesan</p>
+        <p className="mb-4">Tiago Mendes Bottamedi</p>
         <p className="mb-4">Yuri Tobias</p>
     </>
 );
