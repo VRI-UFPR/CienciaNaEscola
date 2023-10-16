de301fdacd66b4f1458b9378f7ee3996c28a1f1c
YuriJT <Yuri.r.tobias@hotmail.com.br>
2023-10-16 12:35:32 -0300

    [ADD]: protocols page

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,31 +1,37 @@
-import React from 'react';
+import { React } from 'react';
+import CheckIcon from '../assets/images/CheckIcon.svg';
 
 const styles = `
-    .homeBtn {
-        background-color: #D9D9D9;
-        width: 85%;
-        height: 60px;
-        padding: 0px 10px;
-        border-radius: 9px;
-        font-size: 70%;
-        box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.5);
-    }
-
     .font-barlow {
         font-family: 'Barlow', sans-serif;
+    }    
+
+    .custom-btn {
+        background-color: #F8F8F8;
+        border-radius: 10px;
+        width: 85%;
+        box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.5);
     }
 `;
 
 function HomeButton(props) {
+    const { title, check } = props;
+
     return (
-        <div className="homeBtn font-barlow d-flex align-items-center">
-            <div className="d-flex justify-content-between flex-fill px-0 py-0">
-                <div>{props.title}</div>
-                <div>17/09/2022</div>
+        <div className="custom-btn d-flex align-items-center font-barlow h-100 px-4">
+            <div className="d-flex align-items-center w-75 h-100 p-0">
+                <h4 className=" text-truncate fw-medium m-0">{title}</h4>
+            </div>
+            <div className="d-flex justify-content-end align-items-center w-25 h-100 p-0">
+                {check && <img src={CheckIcon} alt="Ícone de já respondido" className="w-50 h-50" />}
             </div>
             <style>{styles}</style>
         </div>
     );
 }
+
+HomeButton.defaultProps = {
+    check: false,
+};
 
 export default HomeButton;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,46 +1,30 @@
-import { React, useState, useEffect, useContext, useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import HomeArrows from '../components/HomeArrows';
-import NavBar from '../components/Navbar';
-import RoundedButton from '../components/RoundedButton';
-import HomeButton from '../components/HomeButton';
-import SplashPage from './SplashPage';
+import { React, useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
-import Sidebar from '../components/Sidebar';
-import Alert from '../components/Alert';
+import axios from 'axios';
 
-const styles = `
-    .protocol-info {
-        font-size: 75%;
-        width: 90%;
+import NavBar from '../components/Navbar';
+import SplashPage from './SplashPage';
+import ProtocolCarousel from '../components/ProtocolCarousel';
+
+const style = `
+    .font-barlow {
+        font-family: 'Barlow', sans-serif;
     }
 
-    .general-container {
-        position: absolute;
+    .font-century-gothic {
+        font-family: 'Century Gothic', sans-serif;
     }
 
-    .button-container{
-        padding: 24px 36px;
-    }
-    .home-button-link {
-        text-decoration: none;
-        color: #262626;
-    }
-
-    .list-home-btn { 
-        display: flex;
-        justify-content: center;
-        margin-bottom: 13px;
+    .color-grey {
+        color: #535353;
     }
 `;
 
 function HomePage(props) {
     const [isLoading, setIsLoading] = useState(true);
-    const [userProtocols, setUserForms] = useState([]);
     const { user } = useContext(AuthContext);
-    const navigate = useNavigate();
-    const modalRef = useRef(null);
+
+    const [filteredData, setFilteredData] = useState([]);
 
     useEffect(() => {
         if (user.id !== null && user.token !== null) {
@@ -48,7 +32,7 @@ function HomePage(props) {
             axios
                 .get(`https://genforms.c3sl.ufpr.br/api/user/list/${user.id}`)
                 .then((response) => {
-                    setUserForms(response.data);
+                    setFilteredData(response.data);
                     setIsLoading(false);
                 })
                 .catch((error) => {
@@ -62,38 +46,18 @@ function HomePage(props) {
     }
 
     return (
-        <div className="general-container container-fluid d-flex flex-column font-barlow h-100 w-100 p-0">
-            <NavBar />
-            <div className="d-flex flex-column p-0">
-                <div className="protocol-info container-fluid d-flex justify-content-between mt-4">
-                    <div>Protocolos recentes</div>
-                    <div>Ultima modificação</div>
+        <div className="container-fluid p-0">
+            <div className="row d-flex flex-column align-items-center font-barlow bg-white vh-100 p-0 m-0">
+                <NavBar />
+                <div className="col col-lg-10 d-flex flex-column flex-grow-1 p-4 p-lg-5">
+                    <h1 className="color-grey font-century-gothic fw-bold fs-1 pb-4 m-0">Protocolos</h1>
+                    <div className="d-flex justify-content-center flex-grow-1 pb-5 m-0">
+                        <ProtocolCarousel users={filteredData} />
+                    </div>
                 </div>
+            </div>
 
-                <div className="d-flex container-fluid p-0">
-                    <ul className="container-fluid list-unstyled d-flex flex-column flex-grow-1 p-0 m-0">
-                        {userProtocols.map((userProtocol) => (
-                            <li key={userProtocol.id}>
-                                <Link className="list-home-btn" to={`/protocol/${userProtocol.id}`}>
-                                    <HomeButton title={userProtocol.title} />
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                    <style>{styles}</style>
-                </div>
-                <div className="d-flex">
-                    <HomeArrows />
-                </div>
-            </div>
-            <div className="d-flex button-container flex-grow-1 align-items-end justify-content-end">
-                <RoundedButton role="link" onClick={() => navigate('/help')} />
-            </div>
-            <Alert id="HomePageAlert" ref={modalRef} />
-            <div className={`offcanvas offcanvas-start bg-coral-red w-auto d-flex`} tabIndex="-1" id="sidebar">
-                <Sidebar modalRef={modalRef} showExitButton={true}/>
-            </div>
-            <style>{styles}</style>
+            <style>{style}</style>
         </div>
     );
 }
