8c5a648995679a6cc18ecf1c45d1ef434f7334e6
Matheus <matheusmoraespiovesan@gmail.com>
2023-10-20 09:57:21 -0300

    #209 [FIX]: removed some useless stuff

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,11 +1,9 @@
-import { React, useState, useRef, useContext, useEffect } from 'react';
+import { React, useRef, useContext } from 'react';
 import NavBar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import RoundedButton from '../components/RoundedButton';
 import TextButton from '../components/TextButton';
 import Alert from '../components/Alert';
-// import { useNavigate } from 'react-router-dom';
-import ChangePassword from '../components/ChangePassword';
 import SplashPage from './SplashPage';
 import { AuthContext } from '../contexts/AuthContext';
 import BlankProfilePic from '../assets/images/blankProfile.jpg';
@@ -41,29 +39,14 @@ const profilePageStyles = `
     .profile-label {
         min-width: 5em;
     }
-
-    .underline-light {
-        text-decoration-thickness: 0.05rem;
-    }
-    
-    .update-button:hover {
-        background-color: #0056b3;
-    }
 `;
 
 function ProfilePage(props) {
-    // const [name, setName] = useState('Seu nome');
     const { user } = useContext(AuthContext);
-    const [email, setEmail] = useState(null);
     const { showSidebar, allowEdit } = props;
-    //const navigate = useNavigate();
     const modalRef = useRef(null);
 
-    useEffect(() => {
-        setEmail(user.email);
-    }, [user]);
-
-    if (email === null) {
+    if (user === null) {
         return <SplashPage />;
     }
 
@@ -79,8 +62,8 @@ function ProfilePage(props) {
                     <NavBar showNavTogglerMobile={true} showNavTogglerDesktop={!showSidebar} />
                     <div className="d-flex flex-column flex-grow-1 p-4 p-lg-5">
                         <div className="color-dark-gray pb-4">
-                            <h1 className="font-century-gothic fw-bold fs-3 pb-2 m-0">Seu perfil</h1>
-                            <h2 className="fw-medium fs-5 m-0">Edite e adicione informações sobre você</h2>
+                            <h1 className="font-century-gothic fw-bold fs-3 pb-1 m-0">Seu perfil</h1>
+                            <h5 className="fw-medium m-0">Edite e adicione informações sobre você</h5>
                         </div>
                         <div className="row bg-pastel-blue align-items-center rounded p-4 p-lg-5 m-0">
                             <div className="col-12 col-lg-3 d-flex flex-column align-items-center p-0 pb-4 pb-lg-0">
@@ -88,7 +71,7 @@ function ProfilePage(props) {
                                     <img src={BlankProfilePic} className="rounded-circle h-100 w-100" alt="Foto de perfil" />
                                     <div className="position-absolute d-flex justify-content-end align-items-end">
                                         <RoundedButton
-                                            hsl={[37, 98, 76]}
+                                            hsl={[184, 9, 62]}
                                             role="link"
                                             onClick={() => modalRef.current.showModal({ title: 'Esta função estará disponível em breve.' })}
                                             icon={adicionarFicheiro}
@@ -96,25 +79,12 @@ function ProfilePage(props) {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col d-flex flex-column justify-content-center">
-                                {/* <div className="row align-items-center pb-2 pb-lg-4 m-0">
-                                    <label htmlFor="name-input" className="col-12 col-lg-1 form-label profile-label fs-5 pe-lg-5 mb-0">
-                                        Nome:
-                                    </label>
-                                    <input
-                                        type="text"
-                                        disabled={!allowEdit}
-                                        className="col form-control rounded-4 shadow-sm fs-5"
-                                        id="name-input"
-                                        value={name}
-                                        onChange={(e) => setName(e.target.value)}
-                                    ></input>
-                                </div> */}
+                            <div className="col-12 col-lg-9 d-flex flex-column justify-content-center">
                                 <div className="row align-items-center m-0 mb-2">
                                     <div className="row mb-1">
                                         <label
                                             htmlFor="name-input"
-                                            className="col-12 form-label profile-label fs-5 pb-2 pb-lg-0 pe-lg-5 mb-0"
+                                            className="col-12 form-label profile-label fs-5 pb-0 pe-lg-5 mb-0"
                                         >
                                             Nome:
                                         </label>
@@ -130,7 +100,7 @@ function ProfilePage(props) {
                                     <div className="row mb-1">
                                         <label
                                             htmlFor="username-input"
-                                            className="col-12 form-label profile-label fs-5 pb-2 pb-lg-0 pe-lg-5 mb-0"
+                                            className="col-12 form-label profile-label fs-5 pb-0 pe-lg-5 mb-0"
                                         >
                                             Nome de usuário:
                                         </label>
@@ -139,16 +109,14 @@ function ProfilePage(props) {
                                         type="username"
                                         disabled={!allowEdit}
                                         className="col form-control rounded-4 shadow-sm fs-5"
-                                        id="username-input"
-                                        // value={email}
-                                        // onChange={(e) => setEmail(e.target.value)}
+                                        id="username-input"                                    
                                     ></input>
                                 </div>
                                 <div className="row align-items-center m-0 mb-2">
                                     <div className="row mb-1">
                                         <label
                                             htmlFor="institution-input"
-                                            className="col-12 form-label profile-label fs-5 pb-2 pb-lg-0 pe-lg-5 mb-0"
+                                            className="col-12 form-label profile-label fs-5 pb-0 pe-lg-5 mb-0"
                                         >
                                             Instituição:
                                         </label>
@@ -164,7 +132,7 @@ function ProfilePage(props) {
                                     <div className="row mb-1">
                                         <label
                                             htmlFor="role-input"
-                                            className="col-12 form-label profile-label fs-5 pb-2 pb-lg-0 pe-lg-5 mb-0"
+                                            className="col-12 form-label profile-label fs-5 pb-0 pe-lg-5 mb-0"
                                         >
                                             Papel:
                                         </label>
@@ -177,24 +145,7 @@ function ProfilePage(props) {
                                     ></input>
                                 </div>
                             </div>
-                        </div>
-                        <div className="d-flex justify-center justify-content-center">
-                            <button
-                                type="button"
-                                className="btn text-center text-pastel-blue fs-5"
-                                data-bs-toggle="modal"
-                                data-bs-target="#ChangePassword"
-                            >
-                                Deseja alterar sua senha?
-                            </button>
-                            <div className="modal fade" id="ChangePassword" tabIndex="-1" aria-hidden="true">
-                                <div className="modal-dialog modal-dialog-centered">
-                                    <div className="modal-content bg-transparent border-0">
-                                        <ChangePassword />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>                
+                        </div>                                    
                         <div className="row flex-grow-1 justify-center justify-content-center mx-0">
                             <div className="col-6 col-lg-4 pt-4">
                                 <TextButton className={`px-5 ${allowEdit}`} hsl={[97, 43, 70]} text="Salvar" />
