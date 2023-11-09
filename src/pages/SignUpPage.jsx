96d6e3bdae874ff2ebe2000466c646aa3e604925
tiagomb <113149976+tiagomb@users.noreply.github.com>
2023-11-09 08:22:06 -0300

    Update src/pages/SignUpPage.jsx
    Co-authored-by: YuriTobias <118993772+YuriTobias@users.noreply.github.com>

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -179,7 +179,7 @@ function SignUpPage(props) {
                             </label>
                             <input
                                 id="password-input"
-                                className={`shadow ce-input bg-glacier-blue padding-item rounded-4 text-center fs-5 border-0 w-100 ${
+                                className={`ce-input padding-item shadow border-0 rounded-4 text-center fs-5 bg-glacier-blue w-100 ${
                                     validatePassword() ? 'text-white' : 'text-danger'
                                 }`}
                                 placeholder=""
