375f78054ce3df0acfa3f29428edc68d7c80c7ce
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-11-13 02:41:26 -0300

    [FIX]: Renderização de opções com propriedades errôneas em SelectionInput

--- src/components/inputs/answers/SelectInput.jsx
+++ src/components/inputs/answers/SelectInput.jsx
@@ -7,7 +7,7 @@ const styles = `
 `;
 
 function SelectInput(props) {
-    const options = props.input.options || [];
+    const suggestions = props.input.sugestions || [];
     return (
         <div className="rounded shadow bg-white font-barlow pt-3 px-0 mt-3">
             <div className="row align-items-center justify-content-between m-0 pb-3">
@@ -15,9 +15,9 @@ function SelectInput(props) {
                 <div className="col px-0">
                     <select className="form-select border-0 rounded-3">
                         <option disabled selected label="Selecione uma opção:"></option>
-                        {options.map((option, index) => (
-                            <option key={index} value={option.value}>
-                                {option.label}
+                        {suggestions.map((suggestion, index) => (
+                            <option key={index} value={suggestion.value}>
+                                {suggestion.value}
                             </option>
                         ))}
                     </select>
