1513c631f1ee592923b53e75e35e322ef72cf2a5
Izalorran Bonaldi <iosbonaldi@gmail.com>
2024-07-11 10:08:11 -0300

    #394 [FIX]: Cabeçalhos com informações de licença

--- src/components/GalleryModal.jsx
+++ src/components/GalleryModal.jsx
@@ -1,3 +1,15 @@
+/*
+Copyright (C) 2024 Laboratório Visão Robótica e Imagem
+
+Departamento de Informatica - Universidade Federal do Parana - VRI/UFPR
+
+This file is part of CienciaNaEscola. CienciaNaEscola is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+CienciaNaEscola is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy
+of the GNU General Public License along with CienciaNaEscola.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 import React, { useState, useImperativeHandle, forwardRef, useCallback } from 'react';
 import { Modal, Carousel } from 'bootstrap';
 import RoundedButton from './RoundedButton';
@@ -66,7 +78,7 @@ const GalleryModal = forwardRef((props, ref) => {
                             Figura {modal.currentImage + 1}
                         </h5>
                         <div className="d-flex flex-grow-1 flex-column align-items-end">
-                            <RoundedButton hsl={[355, 78, 66]} icon={iconExit} onClick={hideModal}/>
+                            <RoundedButton hsl={[355, 78, 66]} icon={iconExit} onClick={hideModal} />
                         </div>
                     </div>
 
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,15 @@
+/*
+Copyright (C) 2024 Laboratório Visão Robótica e Imagem
+
+Departamento de Informatica - Universidade Federal do Parana - VRI/UFPR
+
+This file is part of CienciaNaEscola. CienciaNaEscola is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+CienciaNaEscola is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy
+of the GNU General Public License along with CienciaNaEscola.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 export const defaultNewInput = (type) => {
     return {
         text: '',
