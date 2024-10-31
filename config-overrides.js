74279937b38b7b84a452a25988e0ef0f404d4d71
Izalorran Bonaldi <iosbonaldi@gmail.com>
2024-10-31 15:05:03 -0300

    #460 [FIX]: Correções de dependências

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,9 @@ module.exports = function override(config, env) {
     config.resolve.fallback = {
         ...config.resolve.fallback,
         crypto: require.resolve('crypto-browserify'),
+        buffer: require.resolve('buffer/'),
+        stream: require.resolve('stream-browserify'),
+        vm: require.resolve('vm-browserify'),
     };
     return config;
 };
