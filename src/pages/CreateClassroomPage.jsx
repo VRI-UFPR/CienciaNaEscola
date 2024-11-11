f14bede5810f89f2117515c4a6bcb9d6e9bd1348
YuriTobias <118993772+YuriTobias@users.noreply.github.com>
2024-11-11 16:11:58 -0300

    Update src/pages/CreateClassroomPage.jsx
    Co-authored-by: Izalorran Bonaldi <iosbonaldi@gmail.com>

--- src/pages/CreateClassroomPage.jsx
+++ src/pages/CreateClassroomPage.jsx
@@ -357,6 +357,7 @@ function CreateClassroomPage(props) {
                                         className="form-control bg-light-pastel-blue fs-5 border-0 rounded-4 mb-3"
                                         onChange={(e) => setClassroom({ ...classroom, name: e.target.value })}
                                         minLength="3"
+                                        maxLength="20"
                                         required
                                     />
                                 </div>
